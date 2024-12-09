1dbb1f982deea1d307434b6c3cd4c25036c32ac5
rubenb1604 <rubenbertram@gmx.de>
2024-12-09 18:30:17 +0100

    initial

---
+++ app/api/Test/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(req: Request) {
+    let items: string[] = [
+        "Dies ist eine Test Nachricht, welche aus dem Internet kommt!"
+    ];
+
+    const randomIndex = Math.floor(Math.random() * items.length);
+    const randomItem = items[randomIndex];
+    return NextResponse.json({ item: randomItem });
+}
